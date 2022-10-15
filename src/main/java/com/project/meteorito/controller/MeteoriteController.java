8c07c8cf7a557d439bc0b991ab40e0810332caf8
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-10-15 15:30:42 -0300

    Criando metodo de listagem de dados de meteoros

--- src/main/java/com/project/meteorito/controller/MeteoriteController.java
+++ src/main/java/com/project/meteorito/controller/MeteoriteController.java
@@ -1,8 +1,13 @@
 package com.project.meteorito.controller;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.project.meteorito.model.Meteorite;
 import com.project.meteorito.service.MeteoriteService;
 
 @RestController
@@ -12,8 +17,12 @@ public class MeteoriteController {
 	private MeteoriteService service;
 
 	public MeteoriteController(MeteoriteService service) {
-		super();
-		this.service = service;
+ 		this.service = service;
+	}
+	
+	@GetMapping
+	private List<Meteorite> listAll() throws IOException {
+		return service.listAllMetorite();
 	}
 	
 	
