a7a85376974521ed4d33acb6c0815ab5076114aa
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-10-15 15:25:16 -0300

    Listagem de acesso de dados e operacao de conversao

--- src/main/java/com/project/meteorito/service/MeteoriteService.java
+++ src/main/java/com/project/meteorito/service/MeteoriteService.java
@@ -1,5 +1,31 @@
 package com.project.meteorito.service;
 
-public class MeteoriteService {
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import com.project.meteorito.model.Meteorite;
+
+@Service
+public class MeteoriteService {
+   
+	
+	 public List<Meteorite> listAllMetorite() throws IOException {
+		  Reader reader = Files.newBufferedReader(Paths.get("src/main/resources/Meteorite_Landings.csv"));
+		  CsvToBean<Meteorite> listAll = new CsvToBeanBuilder<Meteorite>(reader)
+		     .withType(Meteorite.class)
+		     .build();
+	
+		  List<Meteorite> database = listAll.parse();	
+		  System.out.println(database);
+		  return database;
+		   
+	 }
+	
 }
